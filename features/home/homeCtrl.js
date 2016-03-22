44a2c7b2b2fe14782e0fe87443d3414ca3135a9e
Blake Hagen <blakehagen@gmail.com>
2016-03-21 21:43:26 -0600

    home filter

--- features/home/homeCtrl.js
+++ features/home/homeCtrl.js
@@ -1,10 +1,7 @@
 angular.module('localPair').controller('homeCtrl', function ($rootScope, $scope, $state) {
 
-    $rootScope.searchItem = '';
-
     $scope.submitInput = function () {
         $rootScope.searchItem = $scope.searchItem;
-        $scope.searchItem = '';
         $state.go('expertsList');
     };
 
