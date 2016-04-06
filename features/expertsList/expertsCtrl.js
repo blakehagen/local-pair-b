2890daedd1193bc62b98b6cd8dd5ae9ce1c33deb
Blake Hagen <blakehagen@gmail.com>
2016-04-05 21:43:17 -0600

    message on no data on filter

--- features/expertsList/expertsCtrl.js
+++ features/expertsList/expertsCtrl.js
@@ -3,14 +3,16 @@ angular.module('localPair').controller('expertsCtrl', function ($rootScope, $sco
   $scope.getExperts = function () {
     $scope.experts = mainService.getExperts();
     console.log($scope.experts);
-
-    // for (var i = 0; i < $scope.experts.length; i++) {
-    //   $scope.experts[i].skills = $scope.experts[i].skills.join(', ');
-    //   // console.log($scope.experts[i].skills);
-    // }
   };
 
   $scope.getExperts();
+
+  $scope.refreshExperts = function () {
+    $scope.searchItem = '';
+    $scope.searchLocation = '';
+    $scope.experts = mainService.getExperts();
+    console.log($scope.experts);
+  };
 
   $scope.getProfile = function (id) {
     $window.location.href = '/#/expert/' + id;
